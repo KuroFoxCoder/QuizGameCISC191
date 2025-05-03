41e72c72bbe7dd1aa8a9219e72bd2c56593539a2
KuroFoxCoder <146296750+KuroFoxCoder@users.noreply.github.com>
2025-05-03 13:30:44 -0700

    Delete QuizGameCISC191 directory
    Delete the old version of the code, since the new version has made much more progress

--- QuizGameCISC191/src/quizGame/Topic.java
+++
@@ -1,5 +0,0 @@
-package quizGame;
-
-public abstract class Topic {
-	public abstract String[] getQuestions(); //This will apply to 4 different subclasses that will grab specific text files for each game.
-}
